0b891ba5a3057573bd163531ebb60e96025e1186
Rob <merino@live.com>
2016-06-13 14:21:37 -0400

    First commit

---
+++ app/src/main/java/net/macaws/it/hotelfinderandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package net.macaws.it.hotelfinderandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
