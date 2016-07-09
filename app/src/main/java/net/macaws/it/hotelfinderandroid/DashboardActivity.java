cbce9ecf23dfcf8ce5aedd3bce09b6b4a6185769
Rob <merino@live.com>
2016-07-09 17:33:40 -0400

    Login y dashboar hechos

---
+++ app/src/main/java/net/macaws/it/hotelfinderandroid/DashboardActivity.java
@@ -0,0 +1,60 @@
+package net.macaws.it.hotelfinderandroid;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import net.macaws.it.hotelfinderandroid.LoginActivity;
+import net.macaws.it.hotelfinderandroid.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        SharedPreferences preferences = getSharedPreferences(
+                getString(R.string.app_name), MODE_PRIVATE);
+        String username = preferences.getString("username", "");
+        if (username.isEmpty()) {
+            // Usuario no logeado
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Username: " + username, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_option:
+                logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        SharedPreferences preferences = getSharedPreferences(
+                getString(R.string.app_name), MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove("username");
+        editor.commit();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/net/macaws/it/hotelfinderandroid/LoginActivity.java
@@ -0,0 +1,53 @@
+package net.macaws.it.hotelfinderandroid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = (EditText)findViewById(R.id.editUsername);
+        password = (EditText)findViewById(R.id.editPassword);
+    }
+
+    public void loginClick(View view) {
+
+        Log.d("LoginActivity", "You clicked me!");
+        Log.d("LoginActivity", "Username: " + username.getText());
+        Log.d("LoginActivity", "Password: " + password.getText());
+        String rightUsername = "admin";
+        String rightPassword = "foobar";
+
+        if (rightUsername.equals(username.getText().toString()) &&
+                rightPassword.equals(password.getText().toString())) {
+            Toast.makeText(this, R.string.login_success, Toast.LENGTH_LONG).show();
+            // SharedPreferences hace referencia a un archivo
+            // de configuracion
+            SharedPreferences preferences =
+                    getSharedPreferences(getString(R.string.app_name), // Nombre archivo
+                            Context.MODE_PRIVATE); // Solo nuestra app puede leer esto
+            SharedPreferences.Editor editor = preferences.edit(); // Abrir el archivo para escritura
+            editor.putString("username", username.getText().toString()); // Almacenar el valor
+            editor.commit(); // Guardar cambios
+
+            Intent intent = new Intent(this, DashboardActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG).show();
+        }
+    }
+}
