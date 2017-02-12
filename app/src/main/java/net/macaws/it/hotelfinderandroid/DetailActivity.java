d4a406a560a2ad82858ac37b55857a66246ca318
Roberto Merino <merino@live.com>
2017-02-12 03:18:32 -0400

    Reparado los detalles de los hoteles

--- app/src/main/java/net/macaws/it/hotelfinderandroid/DetailActivity.java
+++ app/src/main/java/net/macaws/it/hotelfinderandroid/DetailActivity.java
@@ -22,24 +22,25 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         TextView titleTextView = (TextView)findViewById(R.id.txtTitle);
-        TextView contentTextView = (TextView)findViewById(R.id.txtContent);
-        //CircleImageView profileImageView = (CircleImageView) findViewById(R.id.profile_image_view);
         ImageView profileImageView = (ImageView) findViewById(R.id.profile_image_view);
         TextView addressTextView = (TextView)findViewById(R.id.txtAddress);
         TextView phoneTextView = (TextView)findViewById(R.id.txtPhone);
         TextView webTextView = (TextView)findViewById(R.id.txtWeb);
         TextView summaryTextView = (TextView)findViewById(R.id.txtSummary);
-
+        TextView roomsTextView = (TextView)findViewById(R.id.txtRooms);
+        TextView singleTextView = (TextView)findViewById(R.id.txtSingle);
+        TextView dobleTextView = (TextView)findViewById(R.id.txtDoble);
         Hotel hotel = (Hotel)(getIntent().getSerializableExtra("HOTEL"));
         if (hotel != null) {
             titleTextView.setText(hotel.getName());
-            contentTextView.setText(hotel.getAddress());
-            //Glide.with(this).load(hotel.getUrlImage()).into(profileImageView);
             Glide.with(this).load(hotel.getUrlImage()).into(profileImageView);
             addressTextView.setText(hotel.getAddress());
             phoneTextView.setText(hotel.getPhone());
             webTextView.setText(hotel.getWeb());
             summaryTextView.setText(hotel.getSummary());
+            roomsTextView.setText(hotel.getRooms());
+            singleTextView.setText(hotel.getSingle());
+            dobleTextView.setText(hotel.getDoble());
         }
     }
 }
